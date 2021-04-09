3b5ed8c2d10d8298b4cb440ae5db913bfc9b5f9f
David Souza dos Santos <david.xvc93@gmail.com>
2021-04-09 17:15:52 -0300

    Adicionado componente de produtos com estrutura inicial

--- StockLiveApp/src/app/app.module.ts
+++ StockLiveApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgToggleModule } from 'ngx-toggle-button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { ProductComponent } from './main/product/product.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- StockLiveApp/src/app/main/product/product.component.ts
+++ StockLiveApp/src/app/main/product/product.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  produtos:any = [
+    {image:"img1.jpg", id:"1", name:"Caneta Bic", description: "Caneta de escritorio da cor azul.", status:"Ativo"},
+    {image:"img2.jpg", id:"2", name:"Lapis", description: "Lapis de escritorio da cor preta.", status:"Ativo"},
+    {image:"img3.jpg", id:"3", name:"Régua", description: "Régu azul de 30cm.", status:"Ativo"},
+    {image:"img4.jpg", id:"4", name:"Caderno", description: "Caderno azul 1 matéria.", status:"Inativo"}
+  ];
+  produtosFiltrados:any = [];
+
+  mostrarImagem = true;
+
+  _filtroLista = '';
+
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+  set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.produtosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.produtos;    
+  }
+
   constructor() { }
 
   ngOnInit() {
+    this.produtosFiltrados = this.produtos;
   }
+
+ 
+ filtrarEventos(filtrarPor: string) { 
+   filtrarPor = filtrarPor.toLocaleLowerCase() 
+   return this.produtos.filter(produto => { 
+     return produto.name.toLocaleLowerCase().includes(filtrarPor) 
+   }) 
+ }
+
+ novoProduto(template:any){
+
+ }
+
+ alternarImagem(){}
+
+ editarProduto(){}
+ excluirProduto(){};
 
 }
